f29f3401ed806dc7464826185318a4b9531fa87e
Farhod <ganiyev@gmail.com>
2026-07-01 11:01:29 +0500

    2-qadam: Arifmetik funksiyalar qo'shildi

--- functions.py
+++ functions.py
@@ -25,3 +25,19 @@ def kopaytma(a, b):
 def bolish(a, b):
     return a / b
 
+
+def max_son(a, b):
+    return max(a, b)
+
+
+def min_son(a, b):
+    return min(a, b)
+
+
+def juftmi(a):
+    return a % 2 == 0
+
+
+def toqmi(a):
+    return a % 2 != 0
+
